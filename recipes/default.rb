2243a03fe3a6470f6d3fdf88cac0e24959bcb9a2
Kuniaki Shimizu <k.shimizu@8d1w.com>
2014-06-13 16:49:54 +0900

    install python and percol globally

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,10 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "python::#{node['python']['install_method']}"
+include_recipe "python::pip"
+include_recipe "python::virtualenv"
+
+python_pip "percol" do
+  action :install
+end
