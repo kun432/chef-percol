ed393eb32b6d117d0a3fb9fd99f94684d275ce1c
Kuniaki Shimizu <k.shimizu@8d1w.com>
2014-06-16 13:48:10 +0900

    set rc.py and add zsh_history_search to .zshrc if exists.

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,11 +16,45 @@ end
 
 if node[:percol][:users].any?
   node[:percol][:users].each do |user|
+
     if node[:etc][:passwd]["#{user}"]
       home_directory = node[:etc][:passwd]["#{user}"][:dir]
-      user_shell     = node[:etc][:passwd]["#{user}"][:dir]
-      log "HOME=#{home_directory}"
-      log "SHELL=#{user_shell}"
+      user_shell     = node[:etc][:passwd]["#{user}"][:shell]
+      gid            = node[:etc][:passwd]["#{user}"][:gid]
+      uid            = node[:etc][:passwd]["#{user}"][:uid]
+      
+      directory "#{home_directory}/.percol.d" do
+        owner uid
+        group gid
+        action :create
+      end
+      
+      template "#{home_directory}/.percol.d/rc.py" do
+        source "rc.py.erb"
+        mode 0644
+        owner uid
+        group gid
+        not_if  { File.exists?("#{home_directory}/.percol.d/rc.py") }
+      end
+      
+      template "#{home_directory}/.percol.d/zsh_history_search" do
+        source "zsh_history_search.erb"
+        mode 0644
+        owner uid
+        group gid
+        not_if  { File.exists?("#{home_directory}/.percol.d/zsh_history_search") }
+      end
+      
+      if /\/zsh$/ =~ user_shell && File.exists?("#{home_directory}/.zshrc")
+        bash "percol zsh history search" do 
+          code <<-EOC
+            echo "[ -f ~/.percol.d/zsh_history_search ] && source ~/.percol.d/zsh_history_search" >> #{home_directory}/.zshrc
+          EOC
+          not_if "grep '.percol.d/zsh_history_search' ~/.zshrc", :environment => { :'HOME' => "#{home_directory}" } 
+        end
+      end
+    else
+      log "user #{user} not found. skipped"
     end
   end
 end
