9a071f382e2264af3ede3d27fc38399fb4fd4f71
Kuniaki Shimizu <k.shimizu@8d1w.com>
2014-06-16 12:00:41 +0900

    get user info from ohai

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,3 +13,14 @@ include_recipe "python::virtualenv"
 python_pip "percol" do
   action :install
 end
+
+if node[:percol][:users].any? do
+  node[:percol][:users].each do |user|
+    if node[:etc][:passwd]["#{user}"]
+      home_directory = node[:etc][:passwd]["#{user}"][:dir]
+      user_shell     = node[:etc][:passwd]["#{user}"][:dir]
+      log "HOME=#{home_directory}"
+      log "SHELL=#{user_shell}"
+    end
+  end
+end
